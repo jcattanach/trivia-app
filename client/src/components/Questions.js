12d23cccab6d77d5e2daa0fd59567ab914b3b02d
Jack Cattanach <jackcattanach@live.com>
2019-03-05 15:56:20 -0600

    switched apis to trivia-api npm package

--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -1,49 +1,88 @@
 import React, { Component } from 'react';
 import axios from 'axios'
-import CurrentQuestion from './CurrentQuestion'
 
-const QUESTION_BASE_URL = 'https://opentdb.com/api.php?amount='
-
-// Create a new component CURRENT_QUESTION and send the current question as a prop
-// and use componentWillRecieveProps lifecycle
-
+const Trivia = require('trivia-api')
+const trivia = new Trivia({ encoding: 'url3986' });
+const categoryArray = [
+{id: 9, name: "General Knowledge"},
+{id: 10, name: "Entertainment: Books"},
+{id: 11, name: "Entertainment: Film"},
+{id: 12, name: "Entertainment: Music"},
+{id: 13, name: "Entertainment: Musicals & Theatres"},
+{id: 14, name: "Entertainment: Television"},
+{id: 15, name: "Entertainment: Video Games"},
+{id: 16, name: "Entertainment: Board Games"},
+{id: 17, name: "Science & Nature"},
+{id: 18, name: "Science: Computers"},
+{id: 19, name: "Science: Mathematics"},
+{id: 20, name: "Mythology"},
+{id: 21, name: "Sports"},
+{id: 22, name: "Geography"},
+{id: 23, name: "History"},
+{id: 24, name: "Politics"},
+{id: 25, name: "Art"},
+{id: 26, name: "Celebrities"},
+{id: 27, name: "Animals"},
+{id: 28, name: "Vehicles"},
+{id: 29, name: "Entertainment: Comics"},
+{id: 30, name: "Science: Gadgets"},
+{id: 32, name: "Entertainment: Cartoon & Animations"}
+]
 
 class Questions extends Component {
 
   state = {
-    questionInfo: [],
+    questions: [],
     currentQuestion: []
   }
 
   getQuestions = () => {
-      axios.get(QUESTION_BASE_URL + '10')
-      .then((response) => {
-        let questionInfo = response.data.results
+
+      let options = {
+        amount: 5,
+        difficulty: 'easy',
+        category: i,
+        type: 'boolean'
+      }
+
+      trivia.getQuestions(options)
+      .then(questions => {
+        return questions.results
+      }).then((array)=>{
+        console.log(array)
         this.setState({
-          questionInfo
+          questions: array
         })
-      }).then(()=>{
-        this.setCurrent()
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+      }
+    )
+    .catch(console.error);
+    }
+
+
+  setCurrent = (num) => {
+    this.setState({
+      currentQuestion: this.state.questionInfo[num]
+    })
   }
 
-  setCurrent = () => {
-    this.setState({
-      currentQuestion: this.state.questionInfo[0]
-    })
+  myTimeout = () => {
+    let i = 0
+    this.setCurrent(i)
+    setTimeout(()=>{
+      i++
+      this.setCurrent(i)
+    }, 2000)
   }
 
 
   render() {
+    //
+    // console.log(this.state.questions)
 
     return (
       <div className="Questions">
         Questions
         <button onClick={this.getQuestions}>Get Questions</button>
-        <CurrentQuestion current={this.state.currentQuestion}/>
       </div>
     );
   }
