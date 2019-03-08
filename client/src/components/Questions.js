62aedf36f5c942212ec3e1cf4b240761a3a32ee5
Jack Cattanach <jackcattanach@live.com>
2019-03-08 14:32:21 -0600

    filtering not working api calls

--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -15,7 +15,6 @@ const categoryArray = [
 {id: 17, name: "Science & Nature"},
 {id: 18, name: "Science: Computers"},
 {id: 19, name: "Science: Mathematics"},
-{id: 20, name: "Mythology"},
 {id: 21, name: "Sports"},
 {id: 22, name: "Geography"},
 {id: 23, name: "History"},
@@ -76,8 +75,6 @@ class Questions extends Component {
 
 
   render() {
-    //
-    // console.log(this.state.questions)
 
     return (
       <div className="Questions">
